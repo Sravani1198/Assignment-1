0f5ea28f15f1f65e1c45013ee570bf13478b951d
Sravani1198 <72152749+Sravani1198@users.noreply.github.com>
2020-11-11 07:48:52 -0700

    Add files via upload

---
+++ RazorAssignCode.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RazorEngine1.Controllers
+{
+    public class AssignmentController : Controller
+    {
+        // GET: Assignment
+        public ActionResult Index1()
+        {
+            return View();
+        }
+    }
+}
